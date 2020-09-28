788314563ffaf763b1009d2047c5818945332cae
Matthew Varona <mjv379@nyu.edu>
2020-09-28 20:38:01 +0400

    Final touches to ticket page

--- nav.js
+++ nav.js
@@ -1,7 +1,14 @@
-const menuIcon = document.querySelector(".burger");
-const navbar = document.querySelector(".navbar");
+window.onload=function(){
+	const menuIcon = document.querySelector(".burger");
+	const navbar = document.querySelector(".navbar");
+	const tixButt = document.querySelector(".tixbutt");
 
-menuIcon.addEventListener('click', () => {
-	navbar.classList.toggle("open");
-	console.log(navbar.classList);
-});
+	menuIcon.addEventListener('click', () => {
+		navbar.classList.toggle("open");
+		console.log(navbar.classList);
+	});
+
+	tixButt.addEventListener('click', () => {
+		alert("Your tickets have been reserved! Please check your email for more details. Thank you for reserving with us!");
+	});
+}
